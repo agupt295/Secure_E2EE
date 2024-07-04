93f50c311b6a859a49e879c7759b0980551ef793
Aryan Gupta <aryangupta@Aryans-MacBook-Pro.local>
2024-07-05 01:48:45 +0530

    commit before starting user class

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from decryption import decrypt_message
 from create_client import new_client
 
 # KMS key ARN of the User you want to send te message to
-# key_arn = 'arn:aws:kms:us-east-1:058264392865:alias/kms-user-key'
-alias_name = "User-1"
-key_arn = new_client(alias_name)
+alias_name = "User-01"
+# key_arn = new_client(alias_name)
+key_arn = 'arn:aws:kms:us-east-1:058264392865:key/762a603a-8d8b-47c1-a9eb-1988160b0e18'
 # Encrypt a message
 plaintext_message = "Hello, this is a secure message."
 ciphertext = encrypt_message(plaintext_message, key_arn)
