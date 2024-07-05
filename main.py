a3aa0e190f59626fc02e46392bb648c5f9815680
Aryan Gupta <aryangupta@Aryans-MacBook-Pro.local>
2024-07-05 12:43:25 +0530

    basic password authentication

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from encryption import encrypt_message
 from decryption import decrypt_message
-from create_client import new_client
+from create_client import new_client, User
 
-# KMS key ARN of the User you want to send te message to
-alias_name = "User-01"
-# key_arn = new_client(alias_name)
-key_arn = 'arn:aws:kms:us-east-1:058264392865:key/762a603a-8d8b-47c1-a9eb-1988160b0e18'
-# Encrypt a message
+alias_name = "User-13-class"
+alias_password = "password"
+# key_arn = 'arn:aws:kms:us-east-1:058264392865:key/762a603a-8d8b-47c1-a9eb-1988160b0e18'
+new_user = new_client(alias_name, alias_password)
 plaintext_message = "Hello, this is a secure message."
-ciphertext = encrypt_message(plaintext_message, key_arn)
+ciphertext = encrypt_message(plaintext_message, new_user.get_alias_kms_arn())
 print(f"Encrypted message: {ciphertext}")
 
-# Decrypt the message
-decrypted_message = decrypt_message(ciphertext, key_arn)
-print(f"Decrypted message: {decrypted_message.decode('utf-8')}")
+enter_password = "password"
+if new_user.check_password(enter_password):
+    decrypted_message = decrypt_message(ciphertext, new_user.kms_arn)
+    print(f"Decrypted message: {decrypted_message.decode('utf-8')}")
+
+else:
+    print("incorrect password")
